bb30163414da7df2c2fd1976273b5b9d8c63ac50
Lokesh Sehgal <lokeshsehgal@Lokeshs-MacBook-Pro.local>
2021-08-13 21:11:53 +0530

    just finished depth layout designing - which i can reuse across screens

--- wallet/wallet/Views/Settings/SettingsScreen.swift
+++ wallet/wallet/Views/Settings/SettingsScreen.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import Neumorphic
 
 struct SettingsRowData : Equatable {
     var id: Int
@@ -30,8 +31,6 @@ enum SettingsDestination: Int {
 
 struct SettingsScreen: View {
     
-    @Environment(\.walletEnvironment) var appEnvironment: ZECCWalletEnvironment
-    
     var generalSection = [SettingsRowData(id:0,title:"Language"),
                           SettingsRowData(id:1,title:"Notifications")]
     var securitySection = [SettingsRowData(id:2,title:"Face ID"),
@@ -43,7 +42,9 @@ struct SettingsScreen: View {
     var aboutSection = [SettingsRowData(id:8,title:"Privacy Policy"),
                         SettingsRowData(id:9,title:"Terms & Conditions"),
                         SettingsRowData(id:10,title:"Support")]
-   
+    
+    @Environment(\.walletEnvironment) var appEnvironment: ZECCWalletEnvironment
+    
     @State var destination: SettingsDestination?
     
     @State var mSelectedSettingsRowData: SettingsRowData?
@@ -54,63 +55,95 @@ struct SettingsScreen: View {
                 
                 VStack(alignment: .center, spacing: 10) {
                     Spacer(minLength: 5)
-                    Text("Settings").font(.barlowRegular(size: 22)).multilineTextAlignment(.center).foregroundColor(.white)
-                    Spacer(minLength: 5)
-                    List {
-                               Section(header: SettingsSectionHeaderView(aTitle: "General").textCase(nil)
-                                .listRowInsets(EdgeInsets(
-                                   top: 0,
-                                   leading: 0,
-                                   bottom: 0,
-                                   trailing: 0))
-                               )
-                              {
-                                  
-                                  ForEach(generalSection, id: \.id) { settingsRowData in
-                                    
-                                    SettingsRow(mCurrentRowData: settingsRowData, mSelectedSettingsRowData: self.$mSelectedSettingsRowData)
-                                   
-                                   }
-                               }
+                    Text("Settings").font(.barlowRegular(size: 20)).multilineTextAlignment(.center).foregroundColor(.white)
+                        
+                    ScrollView {
 
-                               Section(header: SettingsSectionHeaderView(aTitle: "Security").textCase(nil)) {
-                                   ForEach(securitySection, id: \.id) { settingsRowData in
-                                    SettingsRow(mCurrentRowData: settingsRowData, mSelectedSettingsRowData: self.$mSelectedSettingsRowData)
+                        VStack {
+                            ForEach(generalSection, id: \.id) { settingsRowData in
+                                VStack {
+                                    Text(settingsRowData.title).font(.barlowRegular(size: 16)).foregroundColor(.white)
+                                                    .frame(width: 230, height: 22,alignment: .leading)
+                                                    .foregroundColor(Color.white)
+                                        .padding(.trailing, 100)
+                                        .padding()
+                                    
+                                    if settingsRowData.id < 1 {
+                                        Color.gray.frame(height:CGFloat(1) / UIScreen.main.scale)
                                     }
-                               }
-                        
-                                Section(header: SettingsSectionHeaderView(aTitle: "Manage Wallet").textCase(nil)){
-                                   
-                                    ForEach(walletSection, id: \.id) { settingsRowData in
-                                        SettingsRow(mCurrentRowData: settingsRowData, mSelectedSettingsRowData: self.$mSelectedSettingsRowData)
-                                     }
                                 }
+                            }
+                            
+                        }
+                        .modifier(SettingsSectionBackgroundModifier())
                         
-                                Section(header: SettingsSectionHeaderView(aTitle: "About").textCase(nil)){
-                                    ForEach(aboutSection, id: \.id) { settingsRowData in
-                                        SettingsRow(mCurrentRowData: settingsRowData, mSelectedSettingsRowData: self.$mSelectedSettingsRowData)
-                                     }
+                        
+                        VStack {
+                            ForEach(securitySection, id: \.id) { settingsRowData in
+                                VStack {
+                                    Text(settingsRowData.title).font(.barlowRegular(size: 16)).foregroundColor(.white)
+                                                    .frame(width: 230, height: 22,alignment: .leading)
+                                                    .foregroundColor(Color.white)
+                                        .padding(.trailing, 100)
+                                        .padding()
+                                    
+                                    if settingsRowData.id < 5 {
+                                        Color.gray.frame(height:CGFloat(1) / UIScreen.main.scale)
+                                    }
                                 }
+                            }
+                            
+                        }
+                        .modifier(SettingsSectionBackgroundModifier())
                         
-                    }.listStyle(InsetGroupedListStyle())
+                        VStack {
+                            ForEach(walletSection, id: \.id) { settingsRowData in
+                                VStack {
+                                    Text(settingsRowData.title).font(.barlowRegular(size: 16)).foregroundColor(.white)
+                                                    .frame(width: 230, height: 22,alignment: .leading)
+                                                    .foregroundColor(Color.white)
+                                        .padding(.trailing, 100)
+                                        .padding()
+                                    if settingsRowData.id < 7 {
+                                        Color.gray.frame(height:CGFloat(1) / UIScreen.main.scale)
+                                    }
+                                }
+                            }
+                            
+                        }
+                        .modifier(SettingsSectionBackgroundModifier())
+                        
+                        VStack {
+                            ForEach(aboutSection, id: \.id) { settingsRowData in
+                                VStack {
+                                    Text(settingsRowData.title).font(.barlowRegular(size: 16)).foregroundColor(.white)
+                                                    .frame(width: 230, height: 22,alignment: .leading)
+                                                    .foregroundColor(Color.white)
+                                        .padding(.trailing, 100)
+                                        .padding()
+                                    if settingsRowData.id < 10 {
+                                        Color.gray.frame(height:CGFloat(1) / UIScreen.main.scale)
+                                    }
+                                }
+                            }
+                            
+                        }
+                        .modifier(SettingsSectionBackgroundModifier())
+                    }
+                    .background(Color.init(red: 33.0/255.0, green: 36.0/255.0, blue: 38.0/255.0))
+          
                     .onTapGesture {
                         self.destination = SettingsDestination(rawValue: self.mSelectedSettingsRowData?.id ?? 0)
                     }
                 }
-                
-                NavigationLink(
-                    destination: NotificationScreen(),
-                               tag: SettingsDestination.openNotifications,
-                               selection: $destination
-                        
-                ) {
-                    EmptyView()
-                }
+            }.onAppear() {
+//                UITableView.appearance().backgroundColor = UIColor.init(Color.init(red: 25.0/255.0, green: 28.0/255.0, blue: 29.0/255.0))
                 
             }
     }
     
 }
+
 
 
 struct SettingsSectionHeaderView : View {
@@ -134,6 +167,8 @@ struct SettingsRow: View {
         
         ZStack {
             Text(mCurrentRowData.title).font(.barlowRegular(size: 16)).foregroundColor(.white)
+            Color.clear
+                       .ignoresSafeArea()
         }.listRowBackground(Color.zSettingsRowBackground).onTapGesture {
             self.mSelectedSettingsRowData = self.mCurrentRowData
         }
@@ -145,4 +180,19 @@ struct SettingsScreen_Previews: PreviewProvider {
     static var previews: some View {
         SettingsScreen()
     }
+}
+
+struct SettingsSectionBackgroundModifier: ViewModifier {
+
+        var backgroundColor = Color(.systemBackground)
+
+        func body(content: Content) -> some View {
+            content
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(
+                    RoundedRectangle(cornerRadius: 30).fill(Color.init(red: 29.0/255.0, green: 32.0/255.0, blue: 34.0/255.0))
+                        .softInnerShadow(RoundedRectangle(cornerRadius: 30), darkShadow: Color.init(red: 0.06, green: 0.07, blue: 0.07), lightShadow: Color.init(red: 0.26, green: 0.27, blue: 0.3), spread: 0.05, radius: 2))
+                .padding()
+            }
 }
